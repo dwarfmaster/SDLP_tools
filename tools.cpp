a3e17aeeda1dd7740e3f9320e084f6921dd9188e
lucas8 <luc.linux@mailoo.org>
2012-06-10 14:00:20 +0200

    Création de la classe Liberator.
    Fournit une classe libérant une SDL_Surface compatible avec les
    shared_ptr de boost.

--- tools.cpp
+++ tools.cpp
@@ -2,4 +2,20 @@
 
 namespace sdl
 {
+	Liberator::Liberator()
+	{}
+
+	Liberator::Liberator(const Liberator& cp) throw()
+	{}
+
+	Liberator& Liberator::operator=(const Liberator& cp)
+	{
+		return *this;
+	}
+
+	Liberator& Liberator::operator()(SDL_Surface* surf)
+	{
+		SDL_FreeSurface(surf);
+		return *this;
+	}
 };
--- tools.hpp
+++ tools.hpp
@@ -1,12 +1,17 @@
 #ifndef DEF_SDLP_TOOLS
 #define DEF_SDLP_TOOLS
 
+#include <SDL/SDL.h>
+
 namespace sdl
 {
-	class Liberator
+	class Liberator // Classe servant à libérer les SDL_Surface pour les shared_ptr de boost
 	{
 		public:
-		private:
+			Liberator();
+			Liberator(const Liberator& cp) throw();
+			Liberator& operator=(const Liberator& cp);
+			Liberator& operator()(SDL_Surface* surf);
 	};
 };
 
